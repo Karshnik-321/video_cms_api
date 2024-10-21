c81711d4aa9745649df1d94e89de499fd8373700
Purvika Gupta <purvika.gupta@jagrannewmedia.com>
2024-10-21 15:42:26 +0530

    adding editor and campaign

---
+++ Routes/v1/editor/editorRoute.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const editorController = require("../../../Controllers/v1/Editor/editorController");
+console.log(editorController)
+router.route("/editor/insert").post(editorController.create);
+router.route("/editor/update").post(editorController.update);
+router.route("/editor/delete").post(editorController.delete);
+module.exports = router;
--- index.js
+++ index.js
@@ -14,12 +14,13 @@ const version = require("./config/config.json").version;
 var route_path = `./Routes/${version}`;
 // var authMiddleware = require("./middleware/auth").authMiddleware;
 const authRouter = require(route_path+"/Auth/authRoute");
-
-
+const editorRouter = require(route_path+"/editor/editorRoute");
+// console.log(editorRouter)
 // const errorMiddlename = require("./middleware/error-middleware");
 
 app.use("/api/v1", [ 
     authRouter,
+    editorRouter
 ]);
 
 
